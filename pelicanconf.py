d1305cdf11d8d20226303f1170ff46f60fed9607
wenjixin <lietongwei@gmail.com>
2013-08-27 21:26:40 +0800

    add plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+THEME ='bootstrap2'
+DISQUS_SITENAME ='liutongweiblog'
+GOOGLE_ANALYTICS ='UA-43528591-1'
